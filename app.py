872ae797dd505c9351cc54c60a87a0304f0b4a98
F-flipy <fran.flipy@gmail.com>
2022-07-28 01:07:12 +0000

    Aula 4 - Implementação do Banco de Dados

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template     #A partir da biblioteca flask importo a Classe Flask
+from flask import Flask, render_template, redirect, url_for
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("hello")        #criação do aplicativo aapp recebe classe Flask com nome hello
+app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")              # criação de rota
-@app.route("/hello")
-def hello():                #define função
-    return "Hello World"    
+db = SQLAlchemy(app)
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
+
+@app.route("/")
+def index():
+   posts =  Post.query.all()
+   return render_template("index.html", posts=posts)
 	
+@app.route("/populate")
+def populate():
+    user = User(username='Fran', email="g@g.com", password_hash='a')
+    post1 = Post(title="Post 1", body="Texto do Post 1", author=user)
+    post2 = Post(title="Post 2", body="Texto do Post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
